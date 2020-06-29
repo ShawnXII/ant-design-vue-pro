356a81b70f213729188365bc0400b6fca84d6feb
shawnXII <wang492715@foxmail.com>
2020-06-29 17:40:47 +0800

    tijiao

--- src/views/system/strategy/index.vue
+++ src/views/system/strategy/index.vue
@@ -7,9 +7,23 @@
       childrenColumnName="childs">
       <template slot="top">
         <!--搜索框-->
-        <search-table-form :fields="fields" ref="tableSearchForm">
-
-        </search-table-form>
+        <!-- <search-table-form :fields="fields" ref="tableSearchForm" /> -->
+        <!--按钮-->
+        <div class="table-operator">
+          <a-button
+            type="primary"
+            ghost
+            icon="plus"
+            v-action:add>新建自定义策略</a-button>
+          <a-button
+            type="primary"
+            ghost
+            icon="import"
+            v-action:remove
+            disabled
+            style="margin-left:5px;">删除</a-button>
+          <a-input-search placeholder="支持搜索策略名称/描述/备注" style="width: 300px;float:right;" @search="search"/>
+        </div>
       </template>
     </search-table>
   </a-card>
@@ -22,7 +36,7 @@ export default {
     data () {
         return {
            columns: [
-                { title: '策略名', dataIndex: 'username', key: 'username', width: '20%', ellipsis: true },
+                { title: '策略名', dataIndex: 'name', key: 'username', width: '20%', ellipsis: true },
                 { title: '描述', dataIndex: 'phone', key: 'phone', width: '50%', ellipsis: true },
                 { title: '服务类型',
                   dataIndex: 'email',
@@ -31,7 +45,6 @@ export default {
                     { text: 'Joe', value: 'Joe' },
                     { text: 'Jim', value: 'Jim' }
                   ],
-
                   width: '10%',
                 ellipsis: true },
                 { title: '操作', dataIndex: 'action', key: 'action', scopedSlots: { customRender: 'action' }, width: '20%' }
@@ -41,6 +54,11 @@ export default {
             { key: 'organizatioName', type: 'input', placeholder: '支持搜索策略名称/描述/备注' }
           ]
         }
+    },
+    methods: {
+      search () {
+        // 搜索
+      }
     }
 }
 </script>
